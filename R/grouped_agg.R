42f26d8ee05afebd89adf5bb8822c69e4d5631a5
Mirko Thalmann <mirkothalmann@hotmail.com>
2022-05-18 14:37:04 +0200

    change independent to dependent variable

--- R/grouped_agg.R
+++ R/grouped_agg.R
@@ -1,10 +1,10 @@
-grouped_agg <- function(data, g_vars, i_vars) {
+grouped_agg <- function(data, g_vars, d_vars) {
   #' @name grouped_agg
-  #' @title summarize i_vars in data in groups g_vars
+  #' @title summarize d_vars in data in groups g_vars
   #' @description n, nuniqe, mean, and se of grouped tbl
-  #' @param data tbl with g_vars and i_vars as columns
+  #' @param data tbl with g_vars and d_vars as columns
   #' @param g_vars grouping variables
-  #' @param i_vars variables to be aggregated aka independent variables
+  #' @param d_vars variables to be aggregated aka dependent variables
   #' @return the aggregated tbl
   #'
   #' @importFrom magrittr `%>%`
@@ -17,8 +17,8 @@ grouped_agg <- function(data, g_vars, i_vars) {
     group_by(across({{ g_vars }})) %>%
     summarise(
       n = n(),
-      across({{i_vars}}, ~length(unique(.)), .names = "nunique_{.col}"),
-      across({{i_vars}}, mean, .names = "mean_{.col}"),
-      across({{i_vars}}, ~ sd(.)/sqrt(length(.)), .names = "se_{.col}")
+      across({{d_vars}}, ~length(unique(.)), .names = "nunique_{.col}"),
+      across({{d_vars}}, mean, .names = "mean_{.col}"),
+      across({{d_vars}}, ~ sd(.)/sqrt(length(.)), .names = "se_{.col}")
     )
 }
